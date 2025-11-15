dd07f60770cbe828f3edfe86959effca9f91e963
JTRBG123 <jtrbg123@gmail.com>
2025-11-15 12:34:17 -0800

    made a UserDao.java file. There are errors in it right now but they will be resolved when more files get added.

---
+++ app/src/main/java/com/example/project2group1/data/UserDao.java
@@ -0,0 +1,18 @@
+package com.example.project2group1.data;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public class UserDao {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insert(User user);
+
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    User getUser(String username);
+
+    @Query("SELECT COUNT(*) FROM users")
+    int countUsers();
+}
