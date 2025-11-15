5471120335cea8bf4ebb1d7b325e8a2523c6818c
JTRBG123 <jtrbg123@gmail.com>
2025-11-15 15:37:36 -0800

    fixed the user.java file so now the code runs

--- app/src/main/java/com/example/project2group1/data/User.java
+++ app/src/main/java/com/example/project2group1/data/User.java
@@ -1,11 +1,13 @@
 package com.example.project2group1.data;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "users")
 public class User {
     @PrimaryKey
+    @NonNull
     public String username;
 
     public String password;
