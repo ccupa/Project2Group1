0f41ba9e69a9582bd1dd8441b4a47c6ac9c1425d
JTRBG123 <jtrbg123@gmail.com>
2025-12-03 22:04:19 -0800

    added a capitalize method

--- app/src/main/java/com/example/project2group1/PokemonQuizActivity.java
+++ app/src/main/java/com/example/project2group1/PokemonQuizActivity.java
@@ -142,4 +142,10 @@ public class PokemonQuizActivity extends AppCompatActivity {
             }
         }).start();
     }
+
+    private String capitalize(String s) {
+        if (s == null || s.isEmpty()) return s;
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
 }
