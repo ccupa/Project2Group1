b0af7a714baf67d0cc8bcb2429e52e6a05fc2e67
JTRBG123 <jtrbg123@gmail.com>
2025-12-03 22:00:44 -0800

    Added imports I think I will need based on the other quizes

--- app/src/main/java/com/example/project2group1/PokemonQuizActivity.java
+++ app/src/main/java/com/example/project2group1/PokemonQuizActivity.java
@@ -1,4 +1,24 @@
 package com.example.project2group1;
 
-public class PokemonQuizActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public class PokemonQuizActivity extends AppCompatActivity {
 }
