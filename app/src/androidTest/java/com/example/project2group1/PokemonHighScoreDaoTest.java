2001076d331dd93925f9c9bc2bd4bfeb1a42120e
JTRBG123 <jtrbg123@gmail.com>
2025-12-06 12:28:49 -0800

    added a createDb method.

--- app/src/androidTest/java/com/example/project2group1/PokemonHighScoreDaoTest.java
+++ app/src/androidTest/java/com/example/project2group1/PokemonHighScoreDaoTest.java
@@ -13,4 +13,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 public class PokemonHighScoreDaoTest {
+
+    private AppDatabase db;
+    private CategoryHighScoreDao hsDao;
+
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+        // In-memory DB so tests don’t touch your real data
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        hsDao = db.categoryHighScoreDao();
+    }
 }
